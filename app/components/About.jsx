e04ba82dbd7cfb9275502080c955a080b5022e8b
Lorenzo Beltrame <lorenzo.beltrame@gmail.com>
2018-02-05 15:10:24 +0700

    Initial Commit

---
+++ app/components/About.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+
+
+/* var About = React.createClass({
+    render: function() {
+        return(
+            <h3>About Component</h3>
+        )
+    }
+}); */
+
+// stateless functional components
+
+var About = (props) => {
+    return(
+    <h3>About Component</h3>
+    )
+}
+
+module.exports = About;
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+/* var WeatherMessage = (props) => {
+    var {temp, location} = props;
+    return(
+        <h3>It's {temp} in {location}.</h3>
+    )
+} */
+
+// destructuring directly in the parameters.
+var WeatherMessage = ({temp, location}) => {
+    return(
+        <h3>It's {temp} in {location}.</h3>
+    )
+}
+
+module.exports = WeatherMessage;
