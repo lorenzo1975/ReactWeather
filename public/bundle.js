cf72f34fe801ce5116bd1f935f236464449020c9
Lorenzo Beltrame <lorenzo.beltrame@gmail.com>
2018-02-05 15:35:30 +0700

    added copy

--- public/bundle.js
+++ public/bundle.js
@@ -26698,9 +26698,18 @@
 
 	var Examples = function Examples(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'Examples Component!'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'Examples Component!'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Welcome to example components'
+	        )
 	    );
 	};
 
