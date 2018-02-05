67ae68c3518b5f9c720849d63c3fde28bc5b8dad
Lorenzo Beltrame <lorenzo.beltrame@gmail.com>
2018-02-05 15:54:46 +0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,32 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// redirect all traffic to http instead of https
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
+
+
+
+/* var express = require('express');
+
+// Create our app
+var app = express();
 
 app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log('Express server is up on port 3000');
-});
+}); */
